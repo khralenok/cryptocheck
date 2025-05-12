ae136b990588d1b4753603b1575d4a75c1db05cd
Grigorii Khralenok <grigoriikhralenok@Grigoriis-Mac-mini.local>
2025-05-12 10:27:29 +0700

    Add "check" command functionality

--- api/coindesk.go
+++ api/coindesk.go
@@ -1,5 +1,64 @@
 package api
 
-func fetch()(
- //"https://data-api.coindesk.com/asset/v2/metadata?assets=BTC,ETH&asset_lookup_priority=SYMBOL&groups=PRICE&quote_asset=VND"
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
 )
+
+type assetData map[string]map[string]interface{}
+
+func FetchAssetMetadata(symbols []string, quoteAsset string) (assetData, error) {
+	url := fmt.Sprintf("https://data-api.coindesk.com/asset/v2/metadata?assets=%s&asset_lookup_priority=SYMBOL&quote_asset=%s&groups=BASIC,PRICE", strings.Join(symbols, ","), quoteAsset)
+
+	data, err := http.Get(url)
+
+	if err != nil {
+		return nil, errors.New("can't get response from Coindesk API")
+	}
+
+	defer data.Body.Close()
+
+	body, err := io.ReadAll(data.Body)
+
+	if err != nil {
+		return nil, errors.New("can't read the data from Coindesk API response")
+	}
+
+	respJson := make(map[string]interface{})
+
+	err = json.Unmarshal(body, &respJson)
+
+	if err != nil {
+		return nil, errors.New("unexpected response format")
+	}
+
+	normalizedData, err := normalizeData(respJson)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return normalizedData, nil
+}
+
+func normalizeData(data map[string]interface{}) (assetData, error) {
+	normalizedData := make(assetData)
+
+	switch v := data["Data"].(type) {
+	case map[string]interface{}:
+		for key, value := range v {
+			interfacedValue, ok := value.(map[string]interface{})
+			if ok {
+				normalizedData[key] = interfacedValue
+			}
+		}
+	default:
+		return nil, errors.New("unexpected json stucture")
+	}
+
+	return normalizedData, nil
+}
--- commands/check.go
+++ commands/check.go
@@ -2,20 +2,35 @@ package commands
 
 import (
 	"fmt"
+	"strconv"
 
+	"example.com/cryptocheck/api"
 	"example.com/cryptocheck/utils"
 )
 
-func Check(symbols *string, fiat *string)error{
-	listOfSymbols := utils.NormalizedSymbols(*symbols)
+func Check(symbols *string, fiat *string) error {
+	NormalizedSymbols := utils.NormalizedSymbols(*symbols)
 	normalizedFiat, err := utils.NormalizeFiat(*fiat)
 
 	if err != nil {
 		return err
 	}
 
-	for _, value := range listOfSymbols{
-		fmt.Printf("%s: 1000.00 %s\n", value, normalizedFiat)
+	assetMetaData, err := api.FetchAssetMetadata(NormalizedSymbols, normalizedFiat)
+
+	if err != nil {
+		return err
+	}
+
+	for key, value := range assetMetaData {
+		priceAsString := fmt.Sprint(value["PRICE_CONVERSION_VALUE"])
+		price, err := strconv.ParseFloat(priceAsString, 64)
+
+		if err != nil {
+			return err
+		}
+
+		fmt.Printf("%s(%s): %.4f %s\n", value["NAME"], key, price, normalizedFiat)
 	}
 
 	return nil
